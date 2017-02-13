7fcd95cd89878ee680f607e3f3c3bea3958fd136
a2ankitrai <a2.ankitrai@gmail.com>
2017-02-14 03:26:22 +0530

    resume

---
+++ gitcommit.bat
@@ -0,0 +1,20 @@
+@ECHO OFF 
+echo:
+
+echo Staging newly added or modified files... 
+git add .
+echo:
+
+echo Configuring git username and user email...
+git config user.name "a2ankitrai"
+git config user.email a2.ankitrai@gmail.com
+echo:
+
+echo Commiting to local repository....
+git commit -m %1
+echo:
+
+echo pushing to remote repository...
+git push origin master
+echo:
+
